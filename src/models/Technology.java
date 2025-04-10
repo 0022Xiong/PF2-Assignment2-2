51038bb45ebd095bc1d8e2a5e5f1d7cbcd8debaf
jyh-666 <202483920005@nuist.edu.cn>
2025-04-10 17:07:15 +0800

    finish Technology.java

---
+++ src/models/Technology.java
@@ -0,0 +1,59 @@
+package models;
+
+public class Technology {
+    private double price;
+    private String id;
+    private String modelName;
+    private Manufacturer manufacturer;
+
+    public Technology(double price,String id,String modelName,Manufacturer manufacturer){
+        setId(id);
+        setModelName(modelName);
+        setManufacturer(manufacturer);
+        setPrice(price);
+    }
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public Manufacturer getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(Manufacturer manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String toString(){
+        return "id" + id +"\n" +
+               "model name" + modelName +"\n" +
+               "price" + price + "\n" +
+                "name and number of employees" + manufacturer;
+    }
+    public double getInsurancePremium(double exchangeRate){
+        return price*exchangeRate;
+    }
+    public String connectToInternet(String connectStatus){
+        return connectStatus;
+    }
+}
